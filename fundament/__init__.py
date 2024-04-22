0cce7d4337db9b602de88b1f2efb47b5580bbc0f
lonnen <chris.lonnen@gmail.com>
2024-04-22 16:36:25 -0700

    buckle up

--- fundament/__init__.py
+++ fundament/__init__.py
@@ -1,5 +1,7 @@
 import logging
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
