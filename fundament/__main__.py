3859b040f3f61a62e515f5055d27961a72dc25db
lonnen <chris.lonnen@gmail.com>
2024-04-22 19:07:47 -0700

    structure the package to use the cli infrastructure

---
+++ fundament/__main__.py
@@ -0,0 +1,18 @@
+from .cli import cli
+
+import logging
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    cli()
