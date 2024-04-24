d8a2f109895e7cb32208fc83973def19b9e6d48e
lonnen <chris.lonnen@gmail.com>
2024-04-24 16:04:34 -0700

    refactor the run_generation code for this context by removing referencing to argparse data structures

--- fundament/__init__.py
+++ fundament/__init__.py
@@ -1,5 +1,8 @@
 import logging
 
+from accelerate import PartialState
+from accelerate.utils import set_seed
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 logging.basicConfig(
@@ -9,38 +12,41 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def generate_text(letters, model_name_or_path, k, penalty_alpha, p, seed, use_cpu, fp16):
+
+def generate_text(
+    letters, model_name_or_path, k, penalty_alpha, p, seed, use_cpu, fp16
+):
     # Initialize the distributed state.
-    distributed_state = PartialState(cpu=args.use_cpu)
+    distributed_state = PartialState(cpu=use_cpu)
 
-    logger.warning(f"device: {distributed_state.device}, 16-bits inference: {args.fp16}")
+    logger.warning(f"device: {distributed_state.device}, 16-bits inference: {fp16}")
 
-    if args.seed is not None:
-        set_seed(args.seed)
+    if seed is not None:
+        set_seed(seed)
 
     # Initialize the model and tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
 
     # tokenizer = GPT2Tokenizer.from_pretrained(args.model_name_or_path)
     # model = OPTForCausalLM.from_pretrained(args.model_name_or_path)
     # Set the model to the right device
     model.to(distributed_state.device)
 
-    if args.fp16:
+    if fp16:
         model.half()
 
-    logger.info(args)
-    prompt_text = args.prompt if args.prompt else input("Model prompt >>> ")
+    prompt_text = """TODO: FIGURE THIS BIT OUT"""
 
     inputs = tokenizer(prompt_text, return_tensors="pt", add_special_tokens=False)
     inputs = {key: value.to(distributed_state.device) for key, value in inputs.items()}
 
     output_sequences = model.generate(
         **inputs,
-        max_length=args.length + len(inputs["input_ids"][0]),
-        penalty_alpha=args.penalty_alpha,
-        top_k=args.k,
+         # tokens ~= 4 english chars, and valid answers must use all the letters
+        max_length=(len(letters) / 3) + len(inputs["input_ids"][0]),
+        penalty_alpha=penalty_alpha,
+        top_k=k,
     )
 
     generated_sequences = []
@@ -49,14 +55,22 @@ def generate_text(letters, model_name_or_path, k, penalty_alpha, p, seed, use_cp
         generated_sequence = generated_sequence.tolist()
 
         # Decode text
-        text = tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True, add_special_tokens=False)
-
-        # Remove all text after the stop token
-        text = text[: text.find(args.stop_token) if args.stop_token else None]
+        text = tokenizer.decode(
+            generated_sequence,
+            clean_up_tokenization_spaces=True,
+            add_special_tokens=False,
+        )
 
         # Add the prompt at the beginning of the sequence. Remove the excess text that was used for pre-processing
         total_sequence = (
-            prompt_text + text[len(tokenizer.decode(inputs["input_ids"][0], clean_up_tokenization_spaces=True)) :]
+            prompt_text
+            + text[
+                len(
+                    tokenizer.decode(
+                        inputs["input_ids"][0], clean_up_tokenization_spaces=True
+                    )
+                ) :
+            ]
         )
 
         generated_sequences.append(total_sequence)
