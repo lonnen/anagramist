3d1371d0a93c68b9bc18eeb3954fb889756a9429
lonnen <chris.lonnen@gmail.com>
2024-04-25 16:15:22 -0700

    add initial test
    verify the cli version

---
+++ tests/test_anagramist.py
@@ -0,0 +1,13 @@
+import pytest
+
+import anagramist
+from anagramist.cli import cli
+
+from click.testing import CliRunner
+
+def test_version():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["--version"])
+        assert result.exit_code == 0
+        assert result.output.startswith("cli, version ")
