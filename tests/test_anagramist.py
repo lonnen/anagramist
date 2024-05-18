81c999be9df86b32c29256b29b5f5c2b0d08c1d1
lonnen <chris.lonnen@gmail.com>
2024-05-17 21:28:43 -0700

    add several validate_solution tests

--- tests/test_anagramist.py
+++ tests/test_anagramist.py
@@ -9,9 +9,25 @@ def test_generate_text():
 
 def test_validate_solution():
     assert validate_solution("a", "a")
-    assert validate_solution(
-        "But they were also concerned about people stealing their ideas!",
-        "eeeeeeeeeetttttaaaaoooorrrlllsssnnniiiuuhhccddppBywbg!",
-    )
     assert not validate_solution("b", "a")
+
+    assert validate_solution(
+        "abeeefgiklnorrssttt",
+        "letter bank goes first"
+    )
+    assert not validate_solution(
+        "letter bank goes first",
+        "abeeefgiklnorrssttt"
+    )
+
     assert not validate_solution("CAPS MATTER", "caps matter")
+    assert not validate_solution("aaa!!!", "aaa")
+    
+    assert validate_solution("!acinnopttuu", "punctuation!")
+    assert validate_solution("!acinnopttuu", "!punctuation")
+    assert not validate_solution("!acinnopttuu", "punct!uation")
+    
+    assert validate_solution(
+        "eeeeeeeeeetttttaaaaoooorrrlllsssnnniiiuuhhccddppywbbg!",
+        "but they were also concerned about people stealing their ideas!",
+    )
