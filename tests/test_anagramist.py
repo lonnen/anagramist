87735e5f467ea9597f34fc270144c19a62de2e83
lonnen <chris.lonnen@gmail.com>
2024-05-17 20:45:56 -0700

    assert additional properties of validate_solution

--- tests/test_anagramist.py
+++ tests/test_anagramist.py
@@ -6,6 +6,12 @@ from anagramist import validate_solution
 def test_generate_text():
     pass
 
+
 def test_validate_solution():
     assert validate_solution("a", "a")
-    assert validate_solution("But they were also concerned about people stealing their ideas!", "eeeeeeeeeetttttaaaaoooorrrlllsssnnniiiuuhhccddppBywbg!")
+    assert validate_solution(
+        "But they were also concerned about people stealing their ideas!",
+        "eeeeeeeeeetttttaaaaoooorrrlllsssnnniiiuuhhccddppBywbg!",
+    )
+    assert not validate_solution("b", "a")
+    assert not validate_solution("CAPS MATTER", "caps matter")
