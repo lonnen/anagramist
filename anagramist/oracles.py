511c1e457376453d3e54b78169a18e98a6a0e323
lonnen <chris.lonnen@gmail.com>
2024-05-22 19:56:51 -0700

    rename solvers, they don't really solve
    rather, they spit out magic numbers that we will assume are good

--- anagramist/oracles.py
+++ anagramist/oracles.py
@@ -18,7 +18,7 @@ from .logits import LetterBankLogitsProcessor
 logger = logging.getLogger(__name__)
 
 
-class GenerativeSolver:
+class Oracle:
     def __init__(
         self,
         model_name_or_path: str | PathLike[str],
