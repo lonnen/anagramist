eacafc696e0b1d2dd9b797aa5617b660a4af9ea9
lonnen <chris.lonnen@gmail.com>
2024-06-05 15:36:42 -0700

    unfreeze Fragment
    Frozen is getting in the way of assigning strings during instatitation. remove it for now

--- anagramist/fragment.py
+++ anagramist/fragment.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from typing import List
 
 
-@dataclass(frozen=True)
+@dataclass()
 class Fragment:
     """A fragment of the puzzle, consisting of the ordered string and the letters that
     make up that sentence (ignoring spaces, per the rules of the puzzle)
