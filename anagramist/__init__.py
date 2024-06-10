be545f1b5eee5fa1e36b3cd79ed51dfcc889518a
lonnen <chris.lonnen@gmail.com>
2024-06-09 18:59:39 -0700

    document the HeapQueue class

--- anagramist/__init__.py
+++ anagramist/__init__.py
@@ -32,6 +32,21 @@ def search(
 
 
 class HeapQueue(UserList):
+    """A class wrapper around the heapq library.
+
+    Heaps are binary trees for which every parent node has a value less than or equal
+    to any of its children.
+
+    This module follows the python convention of both zero-indexing and pop returning
+    the smallest item, not the largest (called a "min heap").
+
+    By wrapping UserList this integrates the methods of heapq, preserves the invariants,
+    and presents a more typical data structure interface. `heap[0]` is the smallest
+    item, and `heap.sort()` maintains the heap invariant! Not that iterating over the
+    `heap` directly, say, in a list comprehension, will not return items in a sorted 
+    order. Repeatedly popping the heap to exhaustion will, however.
+    """
+
     def __init__(self, iterable: Iterable = []) -> None:
         self.data = [i for i in iterable]
         heapq.heapify(self.data)
