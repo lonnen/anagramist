ddcef38d61bc3f9a97e126fd85f7afd1ab331905
lonnen <chris.lonnen@gmail.com>
2024-06-09 23:55:39 -0700

    add a long winded docstring to Guess acknowledging that the comparison checks should first verify that the guesses come from the same puzzle, but eschew these checks for performance. In the future we may need to add such a check, or find a way to encode the puzzle directly into the type of the Guess

--- anagramist/puzzle.py
+++ anagramist/puzzle.py
@@ -144,7 +144,19 @@ class Puzzle:
 
 @dataclass()
 class Guess:
-    """A guess at the solution"""
+    """A guess at the solution.
+
+    Wraps the placed and remaining letters up with a score.
+
+    The score is used for comparing guesses. As a perf optimization, these comparisons
+    do not check that `Fragment(placed + remaining).letters` are the same in both before
+    comparing scores. Technically guesses with different total letters are from
+    different puzzles and should raise a ValueError or similar. This optimization works
+    only when a guess exists solely in the context of a single Puzzle context.
+
+    Implementors handling guesses from multiple puzzles should implement their own
+    checks to ensures guesses being compared are from the same puzzle.
+    """
 
     placed: str
     remaining: str
