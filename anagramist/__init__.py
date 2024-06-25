91e00e0ca841f367b3034687e5ef41c4f05bc36f
lonnen <chris.lonnen@gmail.com>
2024-06-24 17:53:49 -0700

    start a new search method
    drafting a uct-inspired search method. Since there is only 1 or at most a very small number of winners, this replaces the win-loss ratios of UCT with the Oracle's BEAM score. Once we reach an unscored node, perform some number of uniform-random depth first explorations ending in a win or soft_validation failure. Pick a number that effriciently uses the vram (we can pass ten or maybe a hundred in at a time) and then backpropogate the discovered scores to the graph and start over again from the root. This should give us a UCT-style unbalanced search tree of the space adapted for our weird need-in-a-haystack-search problem

--- anagramist/__init__.py
+++ anagramist/__init__.py
@@ -1,8 +1,11 @@
 import logging
+from random import choices
 from os import PathLike
 
+from .fragment import Fragment
 from .oracles import TransformerOracle
 from .puzzle import Puzzle
+from .searchqueue import PersistentSearchTree
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -39,3 +42,52 @@ def search(
             c1663=c1663,
         )
         return puzzle.search("")
+
+
+# the exploration constant is the stand in score for unscored candidates
+EXPLORATION_SCORE = float(-40)
+
+
+def faux_uct_search(
+    letters: str,
+    model_name_or_path: str | PathLike[str],
+    seed: int,
+    use_gpu: bool = False,
+    fp16: bool = False,
+    c1663: bool = False,
+):
+    # setup
+    oracle = TransformerOracle(model_name_or_path, seed, (not use_gpu), fp16, c1663)
+    search_tree = PersistentSearchTree()
+    puzzle = Puzzle(letters, c1663=c1663)
+    root = "I" if c1663 else ""
+
+    node = root
+    while True:
+        # selection
+        p, r, score, parent = search_tree.get(node)
+        placed = Fragment(placed)
+        remaining = Fragment(remaining)
+
+        if score is None:
+            # we have found an unexpanded node
+            break
+
+        words = []
+        for word in puzzle.vocabulary:
+            if not Fragment(word).letters <= remaining:
+                # word cannot be spelled with remaining characters
+                continue
+            words.append(
+                search_tree.get(placed.sentence + " " + word, EXPLORATION_SCORE)
+            )
+        # weighted random sample based on score, or EXPLORATION_SCORE if unvisited
+        node = choices([w[0] for w in words], weights=[w[1] for w in words])[0]
+        # keep looping until we reach an unexpanded node (no score)
+
+    # expansion
+    node 
+
+    # simulation
+
+    # backpropogation
