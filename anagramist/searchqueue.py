df084a28802ef414687b185da508e41115d209b4
lonnen <chris.lonnen@gmail.com>
2024-06-25 18:39:03 -0700

    initial PersistentSearchTree implementation
    removes the max size, logic related to max size, and random smaple implementation

--- anagramist/searchqueue.py
+++ anagramist/searchqueue.py
@@ -133,3 +133,54 @@ class PersistentSearchQueue:
             (element.placed, element.remaining, element.score),
         )
         con.commit()
+
+
+class PersistentSearchTree:
+    """A persistence structure for storing the tree-search-space in a SQL lite db. 
+    Absolutely not thread-safe, nor any other kind of safe. It will delete all your data
+    and physically deform your storage devices. It is literally powered by injustice.
+
+    Attributes:
+        __TABLE_SCHEMA_VISITED (str): (class attribute) The database schema to be
+        created if the connection is successful and the necessary tables don't exist yet
+        __TABLE_SCHEMA_FRONTIER (str): (class attribute) The database schema to be
+        created if the connection is successful and the necessary tables don't exist yet
+    """
+
+    __TABLE_SCHEMA_VISITED = """
+    CREATE TABLE IF NOT EXISTS visited (
+        placed TEXT NOT NULL,
+        remaining TEXT NOT NULL,
+        parent TEXT NOT NULL,
+        score REAL,
+
+        PRIMARY KEY(placed, remaining)
+    );
+    """
+
+    def __init__(self, db_name="anagramist.db"):
+        self.__db_name = db_name
+        con = sqlite3.connect(self.__db_name)
+        with con:
+            cursor = con.cursor()
+            cursor.execute(self.__TABLE_SCHEMA_VISITED)
+        con.close()
+
+    def __len__(self):
+        with closing(sqlite3.connect(self.__db_name)) as conn:  # auto-closes
+            with conn:  # auto-commits
+                with closing(conn.cursor()) as cursor:  # auto-closes
+                    return cursor.execute("SELECT COUNT(*) FROM visited").fetchone()[0]
+
+    def push(self, element: T):
+        con = sqlite3.connect(self.__db_name)
+        cur = con.cursor()
+        cur.execute(
+            """INSERT INTO visited 
+                VALUES (?, ?, ?) 
+            ON CONFLICT (placed, remaining) 
+            DO UPDATE SET score = excluded.score
+            """,
+            (element.placed, element.remaining, element.score),
+        )
+        con.commit()
