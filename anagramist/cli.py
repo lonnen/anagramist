66175ff7870499016f0a2b6aaa867fd7748b817c
lonnen <chris.lonnen@gmail.com>
2024-07-14 02:41:45 -0700

    add show command
    show can be used to examine the statistics of a single entry. This is useful for understanding how much of the solution space has been explored for a subset of the tree, particularly after repeated application of the trim command

--- anagramist/cli.py
+++ anagramist/cli.py
@@ -1,8 +1,10 @@
 import click
 
-from anagramist.persistentsearchtree import PersistentSearchTree
+from collections import Counter
+from typing import Set
 
-from . import search
+from anagramist.persistentsearchtree import PersistentSearchTree
+from anagramist import search, compute_valid_vocab, vocab, vocab_c1663
 
 
 @click.group()
@@ -71,5 +73,66 @@ def trim(root: str, status):
     click.echo(f"{modified} rows modified. {deleted} rows deleted.")
 
 
+@click.command()
+@click.argument("root")
+@click.option(
+    "-c",
+    "--candidates",
+    type=int,
+    default=5,
+    help="Maximum number of child nodes to show",
+)
+@click.option(
+    "--c1663",
+    is_flag=True,
+    default=True,
+    help="""Leverage additional checks specific to the cryptoanagram puzzle in Dinosaur 
+    comics 1663""",
+)
+def show(root: str, candidates, vocabulary: Set[str] = vocab, c1663: bool = True):
+    click.echo(f"Showing: '{root}'")
+    pst = PersistentSearchTree()
+    placed, remaining, parent, score, cumulative_score, mean_score, status = pst.get(
+        root
+    )
+    children = pst.get_children(root)
+
+    if c1663:
+        vocabulary = vocab_c1663
+    valid_vocab = [w for w in compute_valid_vocab(vocabulary, Counter(remaining))]
+    explored_vocab = {entry[0]: entry for entry in children}
+
+    status_codes = {0: 0, 7: 0}
+    children = []
+    for word in valid_vocab:
+        new_sentence = root + " " + word
+        w = explored_vocab.get(
+            new_sentence,
+            (new_sentence, "", "", None, None, None, "Unexplored"),
+        )
+        sc = w[6]
+        if status_codes.get(sc) is None:
+            status_codes[sc] = 0
+        status_codes[sc] += 1
+        if sc == 0:  # see CANDIDATE_STATUS_CODES for details
+            children.append(w)
+    total = float(sum(status_codes.values()))
+    click.echo(f"Child node demographics ({total:4} children):")
+    click.echo("-----------------------")
+    for sc, v in sorted(status_codes.items(), key=lambda x: str(x)):
+        s = str(sc)[0]
+        percentage = float(v) / total
+        click.echo(f"{s}: {v:4} ({percentage:.2f}%)")
+    click.echo("")
+
+    click.echo("Top next candidates:")
+    click.echo("--------------------")
+    for entry in sorted(children, key=lambda x: x[5], reverse=True)[:candidates]:
+        score = float(entry[5])
+        click.echo(f"{score:.2f} - {entry[0]}")
+    click.echo("")
+
+
 cli.add_command(solve)
 cli.add_command(trim)
+cli.add_command(show)
