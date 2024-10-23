28511412137219008e5ac83726d8844ed8fa2fb2
lonnen <chris.lonnen@gmail.com>
2024-10-23 15:17:10 -0700

    disable most existing commands,  stub out new commands `candidates` and `database`

--- anagramist/cli.py
+++ anagramist/cli.py
@@ -117,116 +117,137 @@ def solve(ctx: click.Context):
 
 
 @click.command()
-@click.argument("root")
-@click.option(
-    "-s",
-    "--status",
-    type=int,
-    default=7,
-    help="Status code for the root node. Non-zero will prevent further searching.",
-)
-@click.option(
-    "-c",
-    "--containing",
-    is_flag=True,
-    help="""Trim all explored nodes containing the 'root' string at each occurance of
-    the root string.""",
-)
-def trim(root: str, status, containing: bool):
-    pst = PersistentSearchTree()
-    if containing:
-        click.echo(f"Trimming all branches containing: '{root}'")
-        modified, deleted = pst.trim_containing(root, status=status)
-    else:
-        click.echo(f"Trimming descendents of: '{root}'")
-        modified, deleted = pst.trim(root, status=status)
-    if modified == 0 and deleted == 0:
-        click.echo(f"Root '{root}' not found in tree.")
-        click.Context.exit(1)
-    if modified == -1 and deleted == -1:
-        click.echo(f"Root '{root}' found but already trimmed. No changes made.")
-        click.Context.exit(1)
-    if modified == -1 and deleted >= 0:
-        click.echo(f"Root '{root}' already has correct status. Deleted {deleted} rows.")
-        click.Context.exit(1)
-    click.echo(f"{modified} rows modified. {deleted} rows deleted.")
+@click.argument("candidate")
+@click.pass_context
+def candidates(
+    ctx: click.Context,
+    candidate: str,
+):
+    click.echo("CANDIDATES - Context:")
+    for k, v in ctx.obj.items():
+        click.echo(f"  {k}: {v}")
+    pass
 
 
 @click.command()
-@click.argument("root")
-@click.option(
-    "-c",
-    "--candidates",
-    type=int,
-    default=5,
-    help="Maximum number of child nodes to show",
-)
-@click.option(
-    "--c1663",
-    is_flag=True,
-    default=True,
-    help="""Leverage additional checks specific to the cryptoanagram puzzle in Dinosaur 
-    comics 1663""",
-)
-def show(root: str, candidates, vocabulary: Set[str] = vocab, c1663: bool = True):
-    click.echo(f"Showing: '{root}'")
-
-    stats, top_children, top_descendents = show_candidate(
-        root,
-        limit=candidates,
-        c1663=c1663,
-    )
-
-    total = float(sum([x["count"] for x in stats.values()]))
-
-    click.echo(f"Child node demographics: ({total:4} children)")
-    click.echo("-----------------------")
-    for sc, v in sorted(stats.items(), key=lambda x: str(x[0])):
-        status = (str(sc)[0],)
-        count = v["count"]
-        percentage = float(v["percentage"]) * 100
-        click.echo(f"{status}: {count:4} ({percentage:.1f}%)")
-    click.echo("")
-
-    click.echo("Top next candidates:")
-    click.echo("--------------------")
-    for entry in top_children.values():
-        score = float(entry[5])
-        click.echo(f"{score:.2f}: {entry[0]}")
-    click.echo("")
-
-    click.echo("Top descendents: (mean score)")
-    click.echo("---------------")
-    for entry in top_descendents.values():
-        score = float(entry[5])
-        click.echo(f"{score:.2f}: {entry[0]}")
-    click.echo("")
+@click.pass_context
+def database(ctx: click.Context):
+    click.echo("CANDIDATES - Database:")
+    for k, v in ctx.obj.items():
+        click.echo(f"  {k}: {v}")
+    pass
 
 
-@click.command()
-@click.argument("words")
-def prune(words: str):
-    """Prune the tree by trimming every occurance of a word or set of words at the
-    occurance of each word
+# @click.command()
+# @click.argument("root")
+# @click.option(
+#     "-s",
+#     "--status",
+#     type=int,
+#     default=7,
+#     help="Status code for the root node. Non-zero will prevent further searching.",
+# )
+# @click.option(
+#     "-c",
+#     "--containing",
+#     is_flag=True,
+#     help="""Trim all explored nodes containing the 'root' string at each occurance of
+#     the root string.""",
+# )
+# def trim(root: str, status, containing: bool):
+#     pst = PersistentSearchTree()
+#     if containing:
+#         click.echo(f"Trimming all branches containing: '{root}'")
+#         modified, deleted = pst.trim_containing(root, status=status)
+#     else:
+#         click.echo(f"Trimming descendents of: '{root}'")
+#         modified, deleted = pst.trim(root, status=status)
+#     if modified == 0 and deleted == 0:
+#         click.echo(f"Root '{root}' not found in tree.")
+#         click.Context.exit(1)
+#     if modified == -1 and deleted == -1:
+#         click.echo(f"Root '{root}' found but already trimmed. No changes made.")
+#         click.Context.exit(1)
+#     if modified == -1 and deleted >= 0:
+#         click.echo(f"Root '{root}' already has correct status. Deleted {deleted} rows.")
+#         click.Context.exit(1)
+#     click.echo(f"{modified} rows modified. {deleted} rows deleted.")
+#
+# @click.command()
+# @click.argument("root")
+# @click.option(
+#     "-c",
+#     "--candidates",
+#     type=int,
+#     default=5,
+#     help="Maximum number of child nodes to show",
+# )
+# @click.option(
+#     "--c1663",
+#     is_flag=True,
+#     default=True,
+#     help="""Leverage additional checks specific to the cryptoanagram puzzle in
+#     Dinosaur comics 1663""",
+# )
+# def show(root: str, candidates, vocabulary: Set[str] = vocab, c1663: bool = True):
+#     click.echo(f"Showing: '{root}'")
 
-    Args:
-        Words - variadic. Either '*' for the c1663_disallow list or a space-separated
-        list of words to be pruned.
-    """
-    to_prune = words.split()
-    if words == "*":
-        click.echo(f"pruning the c1663 dissalow list: {len(c1663_disallow)} entries")
-        to_prune = sorted(c1663_disallow)
-    total_modified, total_deleted = 0, 0
-    pst = PersistentSearchTree()
-    for word in to_prune:
-        click.echo(f"Trimming all branches containing: '{word}'")
-        m, d = pst.trim_containing(word, status=7)
-        click.echo(f"{m} rows modified. {d} rows deleted.")
-        total_modified += m
-        total_deleted += d
-    click.echo(f"Fin. {total_modified} rows modified. {total_deleted} rows deleted.")
-    click.echo("")
+#     stats, top_children, top_descendents = show_candidate(
+#         root,
+#         limit=candidates,
+#         c1663=c1663,
+#     )
+
+#     total = float(sum([x["count"] for x in stats.values()]))
+
+#     click.echo(f"Child node demographics: ({total:4} children)")
+#     click.echo("-----------------------")
+#     for sc, v in sorted(stats.items(), key=lambda x: str(x[0])):
+#         status = (str(sc)[0],)
+#         count = v["count"]
+#         percentage = float(v["percentage"]) * 100
+#         click.echo(f"{status}: {count:4} ({percentage:.1f}%)")
+#     click.echo("")
+
+#     click.echo("Top next candidates:")
+#     click.echo("--------------------")
+#     for entry in top_children.values():
+#         score = float(entry[5])
+#         click.echo(f"{score:.2f}: {entry[0]}")
+#     click.echo("")
+
+#     click.echo("Top descendents: (mean score)")
+#     click.echo("---------------")
+#     for entry in top_descendents.values():
+#         score = float(entry[5])
+#         click.echo(f"{score:.2f}: {entry[0]}")
+#     click.echo("")
+
+
+# @click.command()
+# @click.argument("words")
+# def prune(words: str):
+#     """Prune the tree by trimming every occurance of a word or set of words at the
+#     occurance of each word
+
+#     Args:
+#         Words - variadic. Either '*' for the c1663_disallow list or a space-separated
+#         list of words to be pruned.
+#     """
+#     to_prune = words.split()
+#     if words == "*":
+#         click.echo(f"pruning the c1663 dissalow list: {len(c1663_disallow)} entries")
+#         to_prune = sorted(c1663_disallow)
+#     total_modified, total_deleted = 0, 0
+#     pst = PersistentSearchTree()
+#     for word in to_prune:
+#         click.echo(f"Trimming all branches containing: '{word}'")
+#         m, d = pst.trim_containing(word, status=7)
+#         click.echo(f"{m} rows modified. {d} rows deleted.")
+#         total_modified += m
+#         total_deleted += d
+#     click.echo(f"Fin. {total_modified} rows modified. {total_deleted} rows deleted.")
+#     click.echo("")
 
 
 # @click.command()
@@ -246,6 +267,8 @@ def prune(words: str):
 
 
 cli.add_command(solve)
-cli.add_command(trim)
-cli.add_command(show)
-cli.add_command(prune)
+cli.add_command(candidates)
+cli.add_command(database)
+# cli.add_command(trim)
+# cli.add_command(show)
+# cli.add_command(prune)
