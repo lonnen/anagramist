d0b4706c4d0252cb266057435082e41d89b8115e
lonnen <chris.lonnen@gmail.com>
2024-10-28 23:31:54 -0700

    add tests for the base command and the check-database --help command
    ensure that usage is printed appropriately for the base command and subcommand

--- tests/test_anagramist_cli.py
+++ tests/test_anagramist_cli.py
@@ -9,3 +9,18 @@ def test_version():
         result = runner.invoke(cli, ["--version"])
         assert result.exit_code == 0
         assert result.output.startswith("cli, version ")
+
+def test_base():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, [])
+        assert result.exit_code == 0
+        assert result.output.startswith(
+            "Usage: cli [OPTIONS] COMMAND [ARGS]...")
+
+def test_check_database():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["check-database", "--help"])
+        assert result.exit_code == 0
+        assert result.output.startswith("Usage: cli check-database [OPTIONS]")
