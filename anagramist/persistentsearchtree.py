970ef2a1bc08ebe23ec3df7e4b2a92f053e4870c
lonnen <chris.lonnen@gmail.com>
2024-11-07 17:31:45 -0800

    rewrite all methods of persistentsearchtree to consistently use the context manager for database connections

--- anagramist/persistentsearchtree.py
+++ anagramist/persistentsearchtree.py
@@ -48,97 +48,89 @@ class PersistentSearchTree:
         self.__db_name = db_name
         con = sqlite3.connect(self.__db_name)
         with db_connection_manager(self.__db_name) as con:
-            cursor = con.cursor()
-            cursor.execute(self.__TABLE_SCHEMA_VISITED)
+            con.cursor().execute(self.__TABLE_SCHEMA_VISITED)
 
     def __len__(self) -> int:
-        with closing(sqlite3.connect(self.__db_name)) as conn:  # auto-closes
-            with conn:  # auto-commits
-                with closing(conn.cursor()) as cursor:  # auto-closes
-                    return cursor.execute("SELECT COUNT(*) FROM visited").fetchone()[0]
+        with db_connection_manager(self.__db_name) as con:
+            return con.cursor().execute("SELECT COUNT(*) FROM visited").fetchone()[0]                    
 
     def contains(
         self, word: str, limit: Optional[int] = None, status: Optional[int] = None
     ):
-        with closing(sqlite3.connect(self.__db_name)) as conn:  # auto-closes
-            with conn:  # auto-commits
-                with closing(conn.cursor()) as cursor:  # auto-closes
-                    if status is None:
-                        status = "*"
-                    fetch = cursor.execute(
-                        """
-                        SELECT *
-                        FROM visited
-                        WHERE 
-                            status = ?
-                        AND (placed LIKE ? OR placed LIKE ?)
-                        ORDER BY placed
-                    """,
-                        (status, "% " + word + " %", "% " + word),
-                    ).fetchall()
-                    # SQLite is case insensitive, so double filter for case issues
-                    rows = []
-                    for row in fetch:
-                        placed = row[0]
-                        if f" {word} " in placed or placed.endswith(f" {word}"):
-                            rows.append(row)
-                        else:
-                            pass
-                    # because of post-fetch filtering we cannot apply the limit until
-                    # after
-                    if limit is None:
-                        limit = len(rows)
-                    return rows[:limit]
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            if status is None:
+                status = "*"
+            fetch = cursor.execute(
+                """
+                SELECT *
+                FROM visited
+                WHERE 
+                    status = ?
+                AND (placed LIKE ? OR placed LIKE ?)
+                ORDER BY placed
+            """,
+                (status, "% " + word + " %", "% " + word),
+            ).fetchall()
+            # SQLite is case insensitive, so double filter for case issues
+            rows = []
+            for row in fetch:
+                placed = row[0]
+                if f" {word} " in placed or placed.endswith(f" {word}"):
+                    rows.append(row)
+                else:
+                    pass
+            # because of post-fetch filtering we cannot apply the limit until after
+            if limit is None:
+                limit = len(rows)
+            return rows[:limit]
 
     def get(
         self, placed: str, default=None
     ) -> Tuple[str, str, str, float, float, float, int]:
-        with closing(sqlite3.connect(self.__db_name)) as conn:  # auto-closes
-            with conn:  # auto-commits
-                with closing(conn.cursor()) as cursor:  # auto-closes
-                    fetch = cursor.execute(
-                        """
-                        SELECT *
-                        FROM visited
-                        WHERE placed = ?
-                        LIMIT 1
-                    """,
-                        (placed,),
-                    ).fetchone()
-                    if fetch is None:
-                        if default is not None:
-                            return default
-                    return fetch
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            fetch = cursor.execute(
+                """
+                SELECT *
+                FROM visited
+                WHERE placed = ?
+                LIMIT 1
+            """,
+                (placed,),
+            ).fetchone()
+            if fetch is None:
+                if default is not None:
+                    return default
+            return fetch
 
     def get_children(
         self, parent: str
     ) -> List[Tuple[str, str, str, float, float, float, int]]:
-        with closing(sqlite3.connect(self.__db_name)) as conn:  # auto-closes
-            with conn:  # auto-commits
-                with closing(conn.cursor()) as cursor:  # auto-closes
-                    return cursor.execute(
-                        """
-                        SELECT *
-                        FROM visited
-                        WHERE parent = ?
-                    """,
-                        (parent,),
-                    ).fetchall()
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            return cursor.execute(
+                """
+                SELECT *
+                FROM visited
+                WHERE parent = ?
+            """,
+                (parent,),
+            ).fetchall()
 
     def get_descendents(
         self, parent: str
     ) -> List[Tuple[str, str, str, float, float, float, int]]:
-        with closing(sqlite3.connect(self.__db_name)) as conn:  # auto-closes
-            with conn:  # auto-commits
-                with closing(conn.cursor()) as cursor:  # auto-closes
-                    return cursor.execute(
-                        """
-                        SELECT *
-                        FROM visited
-                        WHERE placed LIKE ?
-                    """,
-                        (parent + " %",),
-                    ).fetchall()
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            return cursor.execute(
+                """
+                SELECT *
+                FROM visited
+                WHERE placed LIKE ?
+            """,
+                (parent + " %",),
+            ).fetchall()
 
     def push(
         self,
@@ -150,58 +142,57 @@ class PersistentSearchTree:
         mean_score: float | None,
         status: int | None,
     ) -> None:
-        con = sqlite3.connect(self.__db_name)
-        cur = con.cursor()
-        cur.execute(
-            """INSERT INTO visited 
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-            ON CONFLICT (placed, remaining) 
-            DO UPDATE SET 
-                score = excluded.score, 
-                cumulative_score = excluded.cumulative_score,
-                mean_score = excluded.mean_score,
-                status = excluded.status
-            """,
-            (placed, remaining, parent, score, cumulative_score, mean_score, status),
-        )
-        con.commit()
-
-    def status(self, placed: str, status: int) -> int:
-        con = sqlite3.connect(self.__db_name)
-        cur = con.cursor()
-        cur.execute(
-            """
-            SELECT 1
-            FROM visited
-            WHERE
-                placed = ?
-            """,
-            (placed,),
-        )
-
-        entry = cur.fetchone()
-
-        if not entry:
-            # nothing found
-            return 0
-
-        if entry[-1] == status:
-            # status is already correctly set
-            return -1
-        else:
-            # mark the root with the appropriate status
-            cur = con.cursor()
-            cur.execute(
-                """
-                UPDATE visited
-                SET status = ?
-                WHERE placed = ?
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            cursor.execute(
+                """INSERT INTO visited 
+                    VALUES (?, ?, ?, ?, ?, ?, ?)
+                ON CONFLICT (placed, remaining) 
+                DO UPDATE SET 
+                    score = excluded.score, 
+                    cumulative_score = excluded.cumulative_score,
+                    mean_score = excluded.mean_score,
+                    status = excluded.status
                 """,
-                (status, placed),
+                (placed, remaining, parent, score, cumulative_score, mean_score, status),
             )
             con.commit()
 
-        return cur.rowcount
+    def status(self, placed: str, status: int) -> int:
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            cursor.execute(
+                """
+                SELECT 1
+                FROM visited
+                WHERE
+                    placed = ?
+                """,
+                (placed,),
+            )
+
+            entry = cursor.fetchone()
+
+            if not entry:
+                # nothing found
+                return 0
+
+            if entry[-1] == status:
+                # status is already correctly set
+                return -1
+            else:
+                # mark the root with the appropriate status
+                cursor.execute(
+                    """
+                    UPDATE visited
+                    SET status = ?
+                    WHERE placed = ?
+                    """,
+                    (status, placed),
+                )
+                con.commit()
+
+            return cursor.rowcount
 
     def trim(self, placed: str) -> int:
         """Deleted all descendents of a given root node `placed`
@@ -213,49 +204,48 @@ class PersistentSearchTree:
             int - an integer indicating how many records were deleted. Negative numbers
             indicate that the root node was not found.
         """
-        con = sqlite3.connect(self.__db_name)
-        cur = con.cursor()
-        rows = cur.execute(
-            """
-            SELECT *
-            FROM visited
-            WHERE
-                placed LIKE ? OR placed = ?
-            ORDER BY placed
-            """,
-            (placed + " %", placed),
-        ).fetchall()
-        con.commit()
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            rows = cursor.execute(
+                """
+                SELECT *
+                FROM visited
+                WHERE
+                    placed LIKE ? OR placed = ?
+                ORDER BY placed
+                """,
+                (placed + " %", placed),
+            ).fetchall()
 
-        deleted = 0
+            deleted = 0
 
-        if len(rows) == 0:
-            # nothing found
-            return -1
+            if len(rows) == 0:
+                # nothing found
+                return -1
 
-        if rows[0][0] == placed:
-            # ignore the entry itself
-            rows = rows[1:]
-        else:
-            # invalid state?
-            # some node is prefixed with `placed`, but not rooted at ''. It could be
-            # that the search was initiated after `placed` as an optimization
-            # e.g. c1663 does not start with the empty string since the first word is
-            # given
-            pass
+            if rows[0][0] == placed:
+                # ignore the entry itself
+                rows = rows[1:]
+            else:
+                # invalid state?
+                # some node is prefixed with `placed`, but not rooted at ''. It could be
+                # that the search was initiated after `placed` as an optimization
+                # e.g. c1663 does not start with the empty string since the first word is
+                # given
+                pass
 
-        # discard the rest
-        cur = con.cursor()
-        rows = cur.executemany(
-            """
-            DELETE FROM visited
-            WHERE placed = ? AND remaining = ?
-            """,
-            [(r[0], r[1]) for r in rows],
-        )
-        deleted = cur.rowcount
-        con.commit()
-        return deleted
+            # discard the rest
+            cur = con.cursor()
+            rows = cur.executemany(
+                """
+                DELETE FROM visited
+                WHERE placed = ? AND remaining = ?
+                """,
+                [(r[0], r[1]) for r in rows],
+            )
+            deleted = cur.rowcount
+            con.commit()
+            return deleted
 
     def verify_integrity(self) -> Tuple[bool, Counter[str, int]]:
         """Verify that the database exists, the program can connect to it, and answer
@@ -272,24 +262,24 @@ class PersistentSearchTree:
                 more than one try the database integity is *not* intact but the details
                 of the entries may be useful in remediating or recovering the db.
         """
-        con = sqlite3.connect(self.__db_name)
-        cur = con.cursor()
-        cur.execute(
-            """
-            SELECT placed, remaining
-            FROM visited
-            """,
-        )
-        bins = Counter()
-        rowcount = 0
-        for placed, remaining in cur:
-            combined = Fragment(placed) + Fragment(remaining)
-            bins.update(("".join(sorted(combined.letters.elements())),))
-            rowcount += 1
-        con.commit()
-        if rowcount < 1:
-            return (True, bins)
-        return (len(bins) == 1, bins)
+        with db_connection_manager(self.__db_name) as con:
+            cursor = con.cursor()
+            cursor.execute(
+                """
+                SELECT placed, remaining
+                FROM visited
+                """,
+            )
+            bins = Counter()
+            rowcount = 0
+            for placed, remaining in cursor:
+                combined = Fragment(placed) + Fragment(remaining)
+                bins.update(("".join(sorted(combined.letters.elements())),))
+                rowcount += 1
+            con.commit()
+            if rowcount < 1:
+                return (True, bins)
+            return (len(bins) == 1, bins)
 
     # def sample(self):
     #     # https://blog.moertel.com/posts/2024-08-23-sampling-with-sql.html
