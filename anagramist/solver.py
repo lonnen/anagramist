9d549bfc6c0f77cd3ddbb40856ee52833f01a607
lonnen <chris.lonnen@gmail.com>
2024-11-18 23:36:22 -0800

    solver.__init__

--- anagramist/solver.py
+++ anagramist/solver.py
@@ -1,6 +1,34 @@
+import logging
+from typing import Optional
+
+from anagramist.fragment import Fragment
+from anagramist.oracles import TransformerOracle
+from anagramist.persistentsearchtree import PersistentSearchTree
+from anagramist.vocab import corpus
+
+logger = logging.getLogger(__name__)
+
+
 class Solver:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        letters: str,
+        search_tree: PersistentSearchTree,
+        oracle: TransformerOracle,
+        c1663: bool = False,
+        max_iterations: Optional[int] = None,
+    ):
+        self.letter_bank = Fragment(letters).letters
+        self.search_tree = search_tree
+        self.oracle = oracle
+
+        self.root = ""
+        if c1663:
+            logger.info("using special constraints for comic 1663")
+            self.root = "I"
+        self.vocabulary = corpus(c1663)
+        logger.info(f"loaded vocab ({len(self.vocabulary)} items)")
+        self.max_iterations = max_iterations
 
     def solve(self) -> None:
         pass
