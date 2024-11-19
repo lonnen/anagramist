9ae586175025782883422b4f7476d4823890104b
lonnen <chris.lonnen@gmail.com>
2024-11-19 00:46:12 -0800

    sketch solver.solve

--- anagramist/solver.py
+++ anagramist/solver.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from typing import Optional
 
 from anagramist.fragment import Fragment
@@ -17,6 +18,7 @@ class Solver:
         oracle: TransformerOracle,
         c1663: bool = False,
         max_iterations: Optional[int] = None,
+        max_time: Optional[int] = None,
     ):
         self.letter_bank = Fragment(letters).letters
         self.search_tree = search_tree
@@ -30,5 +32,42 @@ class Solver:
         logger.info(f"loaded vocab ({len(self.vocabulary)} items)")
         self.max_iterations = max_iterations
 
+        self.current_iteration = 0
+
     def solve(self) -> None:
-        pass
+        start_time = time.time()
+
+        while True:
+            if (
+                self.max_iterations is not None
+                and self.current_iteration >= self.max_iterations
+            ):
+                logging.info(
+                    "Performed %d runs (%d/s), stopping.",
+                    self.max_iterations,
+                    self.max_iterations / (time.time() - start_time),
+                )
+                break
+
+            if (
+                self._max_time is not None
+                and (time.time() - start_time) > self._max_time
+            ):
+                logging.info(
+                    "Timeout after %d seconds, stopping.",
+                    self._max_time,
+                )
+                break
+
+            candidate = self.root
+
+            # selection
+            candidate = self.select(candidate)
+            # expansion
+            candidate = self.expansion(candidate)
+            # assessment
+            candidate = self.assessment(candidate)
+            # backpropogation
+            self.backpropogation(candidate)
+
+            self.current_iteration += 1
