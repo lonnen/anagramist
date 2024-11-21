f442bb42779f696586a6e592a8f18898d70d22f9
lonnen <chris.lonnen@gmail.com>
2024-11-20 21:40:54 -0800

    add solver tests
    currently adds a single test that initialization happens and arguments are processed as expected

---
+++ tests/test_solver.py
@@ -0,0 +1,34 @@
+from collections import Counter
+from anagramist.oracles import TransformerOracle
+from anagramist.persistentsearchtree import PersistentSearchTree
+from anagramist.solver import Solver
+
+PST_DATABASE = "file::memory:?cache=shared"
+TRANSFOMER_MODEL = "microsoft/phi-1_5"
+TRANSFORMER_SEED = 42
+
+
+class TestSolver:
+    def test_init(self):
+        solver = Solver(
+            "dromiceiomimus is a dinosaur",
+            PersistentSearchTree(PST_DATABASE),
+            TransformerOracle(TRANSFOMER_MODEL, TRANSFORMER_SEED),
+        )
+        assert solver.root == ""
+        assert solver.letter_bank == Counter(
+            {
+                "a": 2,
+                "c": 1,
+                "d": 2,
+                "e": 1,
+                "i": 5,
+                "m": 3,
+                "n": 1,
+                "o": 3,
+                "r": 2,
+                "s": 3,
+                "u": 2,
+                " ": 0,
+            }
+        )
