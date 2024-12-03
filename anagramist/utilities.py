ba6df65718a980db8a313e65ad41057244eba484
lonnen <chris.lonnen@gmail.com>
2024-12-02 21:24:36 -0800

    remove geometric-ish mean
    while it intuitively seemed effective, it disagreed with geometric mean in the domain for which it is defined. Also, this style of language model generation is predicated on the probability of any given word sequence arising as the product of conditional next word distributions so we should just use fsum

--- anagramist/utilities.py
+++
@@ -1,7 +0,0 @@
-from statistics import geometric_mean
-from typing import Iterable, SupportsFloat
-
-
-def geometricish_mean(data: Iterable[SupportsFloat]) -> float:
-    offset = abs(min(data)) + 1
-    return geometric_mean([s + offset for s in data]) - offset
