f0896d9b23cdaf2397bd5ded15301718306475da
lonnen <chris.lonnen@gmail.com>
2024-12-04 23:32:09 -0800

    add Candidate dataclass

---
+++ anagramist/candidate.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from dataclasses import dataclass
+from typing import Union
+from anagramist.fragment import Fragment
+
+
+CANDIDATE_STATUS_CODES = {
+    0: "OK",  # or None
+    1: "Fails Validation",
+    5: "Fully Explored",
+    6: "Unexplored",
+    7: "Manual Intervention",
+}
+
+@dataclass()
+class Candidate:
+    """A candidate solution model, complete with all the fields necessary to record
+    this entry in a database.
+    """
+    placed: Fragment
+    remaining: Counter
+    parent: Fragment
+    score: Union[float, None]
+    cumulative_score: Union[float, None]
+    mean_score: Union[float, None]
+    status: Union[float, int]
