e32834adf722c5d167115b35d56aa2ef5c9b77ff
lonnen <chris.lonnen@gmail.com>
2024-12-15 23:04:20 -0800

    disable test_solver.test_solve for being flaky
    even with a fixed seed there's a lot of variance in this output. it takes a long time to run, and at best it shows us that the solver.solve method doesn't raise an exception (rather than demonstrating correctness). This comments it out to disable it in automatic test suites, but leaves it because there may be some value in a developer manually running it occasionally

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -226,21 +226,23 @@ class TestSolver:
         # cannot use words not in the bank, even though the letters are there
         assert not solver.hard_validate("shabba")
 
-    def test_solve(self, temp_database):
-        """this depends on the exact seed. In order for it to complete in a reasonable
-        time it has to be short, which blunts the ability of the oracle to converge
-        search towards the highest scoring outcome. At best, this is a barely convincing
-        test of the happy path through the wiring of the solver task.
-        """
-        expected = "bash bish bosh"
-        solver = Solver(
-            expected,  # the letter bank
-            PersistentSearchTree(db_name=temp_database),
-            SHARED_ORACLE,
-            vocabulary=["bish", "bash", "bosh"],
-            c1663=False,
-        )
-        assert expected == solver.solve()
+    # disable in test suites because its flaky, but leave it for manually running
+    # def test_solve(self, temp_database):
+    #     """this depends on the exact seed. In order for it to complete in a reasonable
+    #     time it has to be short, which blunts the ability of the oracle to converge
+    #     search towards the highest scoring outcome. At best, this is a barely convincing
+    #     test of the happy path through the wiring of the solver task.
+    #     """
+    #     expected = {"bash bish bosh", "bash bosh bish", "bosh bash bish", 
+    #                 "bosh bish bash", "bash bosh bish", "bash bish bosh"}
+    #     solver = Solver(
+    #         expected,  # the letter bank
+    #         PersistentSearchTree(db_name=temp_database),
+    #         SHARED_ORACLE,
+    #         vocabulary=["bish", "bash", "bosh"],
+    #         c1663=False,
+    #     )
+    #     assert solver.solve() in expected
 
     def test_retrieve_candidate(self, temp_database):
         pst = PersistentSearchTree(db_name=temp_database)
