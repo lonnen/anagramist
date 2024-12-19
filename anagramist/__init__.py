9ad13d4d318176682387c8b08d409bd48b0928f9
lonnen <chris.lonnen@gmail.com>
2024-12-18 23:50:50 -0800

    prune dead search code
    the Solver class does the searching now and we can remove this old search code

--- anagramist/__init__.py
+++ anagramist/__init__.py
@@ -14,36 +14,3 @@ logging.basicConfig(
 )
 logging.captureWarnings(capture=True)
 logger = logging.getLogger(__name__)
-
-PROFILING_ITERATIONS = 10
-"""how many iterations of search to do while profiling"""
-
-
-def search(
-    letters: str,
-    search_tree: PersistentSearchTree,
-    oracle: TransformerOracle,
-    c1663: bool = False,
-    do_profiling: bool = True,
-):
-    if do_profiling:
-        with cProfile.Profile() as pr:
-            s = Solver(
-                letters,
-                search_tree,
-                oracle,
-                c1663=c1663,
-                max_iterations=PROFILING_ITERATIONS,
-            )
-            s.solve()
-        with open("profiling_stats.txt", "w") as stream:
-            stats = Stats(pr, stream=stream)
-            stats.strip_dirs()
-            stats.sort_stats("time")
-            stats.dump_stats(".prof_stats")
-            stats.print_stats()
-        return
-    s = Solver(
-        letters, search_tree, oracle, c1663=c1663, max_iterations=PROFILING_ITERATIONS
-    )
-    s.solve()
